a6333a1a9f43bae91d8e7c6e29a4bb4685046379
Nicholas Walton <ngwalton@gmail.com>
2018-07-24 11:53:21 -0500

    Typos, x label, moved aggregation by count/sp, commenting.

--- analysis/analysis_template.R
+++ analysis/analysis_template.R
@@ -1,5 +1,7 @@
 # example template
 
+# show example of adding commong names?
+
 # run if libraries are not installed
 # install.packages("readxl")
 # install.packages("ggplot2")
@@ -18,8 +20,8 @@ setwd("C:/git_repositories/data_template/data")
 
 # if your data are in excel format you can read them with read_excel
 # read_excel returns a tibble which is a data frame with a couple extra features
-# read_excel will generally read dates in as dates reather than stings
-# in this example, we increased guess_max to 2000 (the number rows it checkes
+# read_excel will generally read dates in as dates rather than stings
+# in this example, we increased guess_max to 2000 (the number rows it checks
 # before setting the data type) so that read_excel correctly guessed the
 # "comments" column
 sp <- read_excel(path = "LeaveTree_2018_Masterfile.xlsx", sheet = "Bird",
@@ -29,34 +31,40 @@ env <- read_excel("LeaveTree_2018_Masterfile.xlsx", "Site")
 
 # if your data are in csv format you can read them with read.csv
 # this is the tried and true method of reading data into R
-# "as.is = TRUE" stops read.csv from formating strings as factors
-# it's often easier to do this and then make factors of any collomns needed
+# "as.is = TRUE" stops read.csv from formatting strings as factors
+# it's often easier to do this and then make factors of any columns needed
 # sp <- read.csv("LeaveTree_2018_Masterfile_bird.csv", as.is = TRUE)
 # env <- read.csv("LeaveTree_2018_Masterfile_site.csv", as.is = TRUE)
 
-# formate date as a date
-# dates may also be formated as "%Y-%m-%d"
+# format date as a date
+# dates may also be formatted as "%Y-%m-%d"
 # date_form <- "%m/%d/%Y"
 # sp$date <- as.Date(sp$date , date_form)
 # env$date <- as.Date(env$date , date_form)
 
 
-# limite to singing obs ----
+# basic error checking ----
 
-# optionally, one can limit the analysis to observations of singing birds
-# sp <- sp[sp$type == "S", ]
+# error checking will vary by dataset, but there are some basic things you can
+# expect to check. if this is a data cleaning exersize, you will want to insure
+# all useful fields contain expected/resonable values, and that there are no
+# missing values from required fields.
 
+# there should be no duplicates of sites/ptcount combinations in this data set.
+# if there was more than one year's worth of data for sites, one could add date
+# to the values to check there.
+anyDuplicated(env[, c("site", "ptcount")]) # any value other than 0 indicates dupicates
 
-# aggregate by count and species ----
+# some columns are not optional -- check these for NA values
+check <- c("site", "ptcount", "date", "time")
+anyNA(env[, check])
 
-# not that this will drop information about what type of observations was recorded
+# there are NAs -- check which column and how many
+sapply(env[, check], function(x) sum(is.na(x)))  # two time values missing
 
-# this will result in one row per species/count for each minute/distance that
-# species was detected. To aggretate such that there is only one record for each
-# species per point count, add "minute" and "distance" to "drop".
-drop <- c("type", "comments")
-sp <- sp[, setdiff(names(sp), drop)]
-sp <- aggregate(howmany ~ ., sp, FUN = sum)
+# this may not be important in this case, but checking unique values is often
+# useful to make sure they are as expected
+sort(unique(env$wind))
 
 
 # check dates ----
@@ -71,6 +79,9 @@ ggplot(data = env, mapping = aes(x = date)) +
 
 # check for large values ----
 
+# plotting the number of individuals for each species from all point counts is
+# good way to spot values that are outside the norm.
+
 # only species counts greater than cutoff will be labeled
 cutoff <- 20
 
@@ -83,7 +94,26 @@ sp_labs <- sp_lv[sp_lv$howmany > cutoff, ]
 ggplot(data = sp_lv, mapping = aes(x = rnum, y = howmany)) +
   geom_point() +
   geom_text(aes(x = rnum, y = howmany, label = sppcode), data = sp_labs,
-            vjust = 1.2)
+            vjust = 1.2) +
+  xlab("row number")
+
+
+# limit to singing obs ----
+
+# optionally, one can limit the analysis to observations of singing birds
+# sp <- sp[sp$type == "S", ]
+
+
+# aggregate by count and species ----
+
+# not that this will drop information about what type of observations was recorded
+
+# this will result in one row per species/count for each minute/distance that
+# species was detected. To aggregate such that there is only one record for each
+# species per point count, add "minute" and "distance" to "drop".
+drop <- c("type", "comments")
+sp <- sp[, setdiff(names(sp), drop)]
+sp <- aggregate(howmany ~ ., sp, FUN = sum)
 
 
 # species frequency distribution ----
@@ -107,7 +137,7 @@ ggplot(data = sp_bar, mapping = aes(x = sppcode, y = howmany)) +
 
 # side-by-side bar plot ----
 
-# here, we predend that there are two treatments in the study Y and N.
+# here, we pretend that there are two treatments in the study Y and N.
 env$treatment <- sample(c("Y", "N"), size = nrow(env), replace = TRUE)
 
 # check how many of each there are
@@ -115,7 +145,9 @@ table(env$treatment)
 
 # this is only needed if you want control over the order that treatment is
 # plotted in. it will order treatment alphabetically by default. one could do
-# the same thing with species if desired
+# the same thing with species if desired. levels must contain all levels in
+# treatment, and should be in the order you want them to appear in the figure
+# legend.
 env$treatment <- factor(env$treatment, levels = c("Y", "N"))
 
 sp_side <- aggregate(howmany ~ site + ptcount + date + sppcode, sp, FUN = sum)
@@ -132,19 +164,19 @@ ggplot(sp_side, aes(x = sppcode, fill = treatment)) +
   # each other
   geom_bar(position = "dodge") +
   # the rest is just making it look nice
-  # not needed in this example, but usefull for long names
+  # not needed in this example, but useful for long names
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ylab("n observations") +
   xlab("Species") +
   scale_fill_manual(values = c("blue3", "firebrick3"))
 
 
-# wide formate ----
+# wide format ----
 
 # often, our data are stored such that a given survey has multiple records
-# associated with it (e.g., one row per species). this is often refered to as
-# long formate. for some analyses, we need one row per survey and one column per
-# species. this is often refered to as wide format. the following converts long
+# associated with it (e.g., one row per species). this is often referred to as
+# long format. for some analyses, we need one row per survey and one column per
+# species. this is often referred to as wide format. the following converts long
 # format to wide format.
 
 # in the formula, place all columns that identify a single survey (site, ptcount,
